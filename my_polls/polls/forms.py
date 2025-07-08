5759c239fd6a44cc835d960a22282764c5612841
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2025-07-08 15:52:35 +0700

    chore: make new style layout

--- my_polls/polls/forms.py
+++ my_polls/polls/forms.py
@@ -18,9 +18,14 @@ class ChoiceModelForm(forms.ModelForm):
         required=True,
         max_length=255,
     )
+    
     class Meta:
         model = Choice
         fields = ['choice_text']
         widgets = {
-            'choice_text': forms.TextInput(attrs={'class': 'form-control'}),
+            'choice_text': forms.TextInput(attrs={'class': 'form-control', 'required': 'required'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['choice_text'].widget.attrs.update({'class': 'form-control', 'required': 'required'})
